21c164542ded270d1222f4666970b5981347bcf7
PabloShim1 <pashashimko0306@gmail.com>
2026-02-25 18:07:54 +0300

    Cleanup and center model

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -1,35 +1,29 @@
 #include "DetectorConstruction.hh"
-#include "G4RunManager.hh"
 #include "G4NistManager.hh"
-#include "G4Box.hh"
-#include "G4LogicalVolume.hh"
 #include "G4PVPlacement.hh"
 #include "G4SystemOfUnits.hh"
 #include "G4VisAttributes.hh"
-#include "G4Color.hh"
-
-// Подключаем CADMesh
 #include "CADMesh.hh"
 
 namespace tyone
 {
     DetectorConstruction::DetectorConstruction()
+        : G4VUserDetectorConstruction()
     {
+        // Инициализация размеров
         this->margin_x = 100 * cm;
         this->margin_y = 100 * cm;
         this->margin_z = 100 * cm; 
+        this->source_surface_distance = 0;
     }
 
-    DetectorConstruction::~DetectorConstruction()
-    {
-    }
+    DetectorConstruction::~DetectorConstruction() {}
 
     void DetectorConstruction::ConstructMaterials()
     {
         G4NistManager *nist = G4NistManager::Instance();
         this->world_material = nist->FindOrBuildMaterial("G4_AIR");
-        nist->FindOrBuildMaterial("G4_POLYETHYLENE");
-        nist->FindOrBuildMaterial("G4_Al");
+        this->glove_material = nist->FindOrBuildMaterial("G4_POLYETHYLENE");
     }
 
     void DetectorConstruction::ComputeTranslations()
@@ -41,7 +35,7 @@ namespace tyone
 
     void DetectorConstruction::ConstructSolids()
     {
-        this->world_solid = new G4Box("World", 
+        this->world_solid = new G4Box("World_Solid", 
                                       this->world_size_x / 2.0, 
                                       this->world_size_y / 2.0, 
                                       this->world_size_z / 2.0);
@@ -49,75 +43,57 @@ namespace tyone
 
     void DetectorConstruction::ConstructLogicals()
     {
-        G4NistManager *nist = G4NistManager::Instance();
-        
-        // 1. Создаем мир
-        this->world_logic = new G4LogicalVolume(this->world_solid, this->world_material, "World");
+        // 1. Логика мира
+        this->world_logic = new G4LogicalVolume(this->world_solid, this->world_material, "World_Logic");
 
-        // 2. ЗАГРУЗКА ПЕРЧАТКИ (КРАСНАЯ, СЛЕВА)
+        // 2. Загрузка и центрирование модели через CADMesh
         auto mesh = CADMesh::TessellatedMesh::FromOBJ("glove1.obj");
         
-        // Увеличиваем масштаб, так как в логе координаты были около 0.5
-        mesh->SetScale(10); 
+        mesh->SetScale(10.0); // Твой масштаб
+        mesh->SetOffset(G4ThreeVector(0, 0, 0)); // Центрируем меш внутри его солида
 
         G4VSolid* glove_solid = mesh->GetSolid();
-        G4LogicalVolume* glove_logic = new G4LogicalVolume(
+        
+        this->glove_logic = new G4LogicalVolume(
             glove_solid, 
-            nist->FindOrBuildMaterial("G4_POLYETHYLENE"), 
+            this->glove_material, 
             "Glove_Logic"
         );
 
-        // Цвет: Ярко-красный, непрозрачный
+        // Визуализация: оставляем красный, раз тебе он нравится, но без лишних объектов
         G4VisAttributes* glove_vis = new G4VisAttributes(G4Colour(1.0, 0.0, 0.0, 1.0));
         glove_vis->SetForceSolid(true); 
-        glove_logic->SetVisAttributes(glove_vis);
+        this->glove_logic->SetVisAttributes(glove_vis);
+    }
 
-        // Размещаем СЛЕВА (-20 см)
+    void DetectorConstruction::ConstructPhysicals()
+    {
+        // Размещаем Мир
+        this->world_phys = new G4PVPlacement(
+            nullptr,
+            G4ThreeVector(),
+            this->world_logic,
+            "World_Phys",
+            nullptr,
+            false,
+            0,
+            true);
+
+        // Размещаем Перчатку точно в центре (0,0,0) мира
         new G4PVPlacement(nullptr, 
                           G4ThreeVector(0, 0, 0), 
-                          glove_logic, 
+                          this->glove_logic, 
                           "Glove_Phys", 
                           this->world_logic, 
                           false, 
                           0, 
                           true);
-
-        // 3. ТЕСТОВЫЙ КУБ (СИНИЙ, СПРАВА)
-        G4Box* box_solid = new G4Box("TestBox", 5*cm, 5*cm, 5*cm);
-        G4LogicalVolume* box_logic = new G4LogicalVolume(box_solid, nist->FindOrBuildMaterial("G4_Al"), "Box_Logic");
-        
-        G4VisAttributes* box_vis = new G4VisAttributes(G4Colour(0.0, 0.0, 1.0, 0.4));
-        box_vis->SetForceSolid(true);
-        box_logic->SetVisAttributes(box_vis);
-
-        // Размещаем СПРАВА (+20 см)
-        new G4PVPlacement(nullptr, 
-                          G4ThreeVector(20*cm, 0, 0), 
-                          box_logic, 
-                          "Box_Phys", 
-                          this->world_logic, 
-                          false, 
-                          0, 
-                          true);
-    }
-
-    void DetectorConstruction::ConstructPhysicals()
-    {
-        this->world_phys = new G4PVPlacement(
-            nullptr,
-            G4ThreeVector(),
-            this->world_logic,
-            "World",
-            nullptr,
-            false,
-            0,
-            true);
     }
 
     void DetectorConstruction::SetUpVisAttributes()
     {
         G4VisAttributes *world_vis = new G4VisAttributes();
-        world_vis->SetVisibility(false); 
+        world_vis->SetVisibility(false); // Делаем мир невидимым, чтобы не мешал
         this->world_logic->SetVisAttributes(world_vis);
     }
 
